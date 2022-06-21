221e55ca3309987822132afefd470284ab7d5538
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-06-21 19:01:04 +0500

    docs: done with controller

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,11 +1,31 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+    @Post()
+    create(@Body() createCatDto: CreateCatDto) {
+        return 'This action adds a new cat';
+    }
+
     @Get()
-    findAll(@Req() request: Request): string {
-        return 'this section return all cats';
+    findAll() {
+        return `This action returns all cats tems)`;
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action returns a #${id} cat`;
+    }
+
+    //   @Put(':id')
+    //   update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    //     return `This action updates a #${id} cat`;
+    //   }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} cat`;
     }
 }
---
+++ src/create-cat.dto.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export class CreateCatDto {
+    name: string;
+    age: number;
+    breed: string;
+}
